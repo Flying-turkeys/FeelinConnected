ddae87aef0f218a5bc98343eb0f8e583a5815f39
ethanmcf <ethan_mcfarland@outlook.com>
2023-03-17 20:27:41 -0400

    Created outline structure for the graph as well as renamed the file main file to our AI players file

---
+++ AI_players.py
@@ -0,0 +1,8 @@
+"""CSC111 Winter 2023 Final Project: Feelin Connected
+
+File Information
+===============================
+This file contains the data classes that will compose the AI used to create the Connect 4 game.
+
+This file is Copyright (c) 2023 Ethan McFarland, Ali Shabani, Aabha Roy and Sukhjeet Singh Nar.
+"""
---
+++ board.py
@@ -0,0 +1,75 @@
+"""CSC111 Winter 2023 Final Project: Feelin Connected
+
+File Information
+===============================
+This file contains the data classes that will compose the graph used to create the Connect 4 game.
+
+This file is Copyright (c) 2023 Ethan McFarland, Ali Shabani, Aabha Roy and Sukhjeet Singh Nar.
+"""
+
+from __future__ import annotations
+from typing import Optional
+from python_ta.contracts import check_contracts
+
+
+@check_contracts
+class Piece:
+    """A piece that represents a piece in a Connect 4 board.
+
+    Instance Attributes
+        - player:
+            The player who this piece belongs to. Can be None if no player has
+            made a move in the location of the piece.
+        - location:
+            The address (i.e., unique identifier) of this node.
+        - connections:
+            A list containing the connections for this piece.
+
+    Representation Invariants:
+        - self.player in {'P1', 'P2', None}
+        - all(point >= 0 for point in self.location)
+        - all(self in conn.endpoints for conn in self.connections)
+    """
+    player: Optional[str]
+    location: tuple[int, int]
+    connections: set[Connection]
+
+
+@check_contracts
+class Connection:
+    """A link (or "edge") connecting two pieces on a Connect 4 board.
+
+    Instance Attributes:
+        - type: The direction (type) of connection two pices make.
+        - endpoints: The two pieces that are linked by this connection.
+
+    Representation Invariants:
+        - len(self.endpoints) == 2
+        - self.type in {'vertical', 'horizontal', 'left-diagnol', 'right-diagnol'}
+    """
+    type: str
+    endpoints: set[Piece]
+
+
+@check_contracts
+class Board:
+    """A graph that represents a Connect 4 board and holds all empty and non-empty spaces/pieces.
+
+    Instance Attributes:
+        - type: The direction (type) of connection two pices make.
+        - endpoints: The two pieces that are linked by this connection.
+
+    Representation Invariants:
+        - len(self.player_moves) == 2
+        - all(type in {'P1', 'P2'} for type in self.player_moves)
+        - all(isinstance(moves, int) for address in self._nodes)
+    """
+    moves: set[Piece]
+    player_moves: dict[str, set[Piece]]
+
+    def __init__(self, width: int) -> None:
+        """Initialize this board with the dimensions of width by width.
+
+        Preconditions:
+            - k >= 5
+        """
