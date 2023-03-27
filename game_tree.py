0a873c596433b839b3c37e97487d182c65b5164a
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-03-27 13:11:46 -0400

    game_tree.py, players.py, modified

--- game_tree.py
+++ game_tree.py
@@ -6,10 +6,11 @@ This file contains the tree used for the AI.
 
 This file is Copyright (c) 2023 Ethan McFarland, Ali Shabani, Aabha Roy and Sukhjeet Singh Nar.
 """
-
+from __future__ import annotations
+from typing import Optional
 from board import Board
 
-
+GAME_START_MOVE = '-'
 class GameTree:
     """A decision tree for Connect 4 game.
 
@@ -23,7 +24,9 @@ class GameTree:
         - GAME_START_MOVE not in self._subtrees  # since it can only appear at the very top of a game tree
         - 0.0 <= self.guesser_win_probability <= 1.0
     """
-    move: list[list[int]]  # The vertical bar | means "or"
+    move: tuple[int, int]
+    player_winning_probability: dict[str, float]
+    _subtrees: dict[tuple[int, int], GameTree]
 
     # Private Instance Attributes:
     #  - _subtrees:
@@ -32,3 +35,31 @@ class GameTree:
     #      this collection is a MAPPING where the values are GameTrees, and associated
     #      keys are the moves at the root of each subtree. See the last representation
     #      invariant above.
+    def __init__(self, move: tuple[int, int] = GAME_START_MOVE,
+                 p1_win_prob: float = 0.0, p2_win_prob: float = 0.0) -> None:
+        """Initialize a new game tree.
+
+        Note that this initializer uses optional arguments.
+
+        >>> game = GameTree()
+        >>> game.move == GAME_START_MOVE
+        True
+        """
+        self.move = move
+        self._subtrees = {}
+        self.player_winning_probability['P1'] = p1_win_prob
+        self.player_winning_probability['P2'] = p2_win_prob
+
+    def get_subtrees(self) -> list[GameTree]:
+        """Return the subtrees of this game tree."""
+        return list(self._subtrees.values())
+
+    def find_subtree_by_move(self, move: tuple[int, int]) -> Optional[GameTree]:
+        """Return the subtree corresponding to the given move.
+
+        Return None if no subtree corresponds to that move.
+        """
+        if move in self._subtrees:
+            return self._subtrees[move]
+        else:
+            return None
--- players.py
+++ players.py
@@ -8,47 +8,53 @@ This file is Copyright (c) 2023 Ethan McFarland, Ali Shabani, Aabha Roy and Sukh
 """
 
 from board import Board
+from board import Piece
+import copy
+import random
 
 
 class AbstractPlayer:
-    """Abstract data type for a specific player playing the game"""
+    """Abstract class representing a specific player playing the game"""
     status: Board
 
-    def __init__(self) -> None:
+    # def __init__(self) -> None:
+    #
+    #     """Abstract method for initializing this player"""
+    #     raise NotImplementedError
 
-        """Abstract method for initializing this player"""
-        raise NotImplementedError
-
-    def make_move(self) -> None:
+    def make_move(self, board: Board) -> None:
         """Abstract method for a player making a move on the board"""
         raise NotImplementedError
 
 
 class Person(AbstractPlayer):
     """Abstract player who represents a real person playing"""
-    def __init__(self, board: Board) -> None:
-        """Abstract method for initializing this player"""
 
-    def make_move(self) -> None:
+    # def __init__(self, board: Board) -> None:
+    #     """Abstract method for initializing this player"""
+
+    def make_move(self, board: Board) -> None:
         """Abstract method for a player making a move on the board"""
 
 
-class RandomGuesser(AbstractPlayer):
-    """Abstract player which uses random guesses to play"""
-    def __init__(self, board: Board) -> None:
-        """Abstract method for initializing this player"""
+class RandomPlayer(AbstractPlayer):
+    """Abstract player which uses random moves to play"""
 
-    def make_move(self) -> None:
+    def make_move(self, board: Board) -> Piece:
         """Abstract method for a player making a move on the board"""
+        possible_moves = board.possible_moves()
+        return random.choice(list(possible_moves))
 
 
-class GreedyGuesser(AbstractPlayer):
+class GreedyPlayer(AbstractPlayer):
     """Abstract player which uses AI to play"""
-    def __init__(self, board: Board) -> None:
-        """Abstract method for initializing this player"""
 
-    def make_move(self) -> None:
+    # def __init__(self, board: Board) -> None:
+    #     """Abstract method for initializing this player"""
+
+    def make_move(self, board: Board) -> None:
         """Abstract method for a player making a move on the board"""
+
 
 
 if __name__ == '__main__':
