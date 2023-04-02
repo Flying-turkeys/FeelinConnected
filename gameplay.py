8b29f850d0e6bbd4033aa6d7c6a8c2d5fa8ca241
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-04-02 16:37:16 -0400

    cleaned the code and added text bubbles

--- gameplay.py
+++ gameplay.py
@@ -7,7 +7,6 @@ This file is Copyright (c) 2023 Ethan McFarland, Ali Shabani, Aabha Roy and Sukh
 """
 import pygame
 from board import Board, Piece
-from typing import Optional
 from players import GreedyPlayer, generate_game_tree
 
 
@@ -36,12 +35,12 @@ class GameBoard(Board):
     """
     # Private Instance Attributes:
     #   - _proportionality: an integer which dicates the size of each item on the board, since it
-    #      is proproptional to every surface.
+    #      is proproptional to everything on the pygame surface.
 
     slots: list[pygame.Rect]
     column_spaces: dict[float: list[float]]
-    game_state = str
-    _proportionality = int
+    game_state: str
+    _proportionality: int
 
     def __init__(self, width: int = 7) -> None:
         """Intialize the game board with the dimentions of width by width (default 7),
@@ -53,7 +52,7 @@ class GameBoard(Board):
         self.column_spaces = {}
         self.game_state = 'unspecified'
 
-    def draw_slots(self, surface: pygame.Surface):
+    def draw_slots(self, surface: pygame.Surface) -> None:
         """Draw each slot of the gameboard on the pygame surface with respects to its width."""
 
         ################################################################
@@ -61,7 +60,8 @@ class GameBoard(Board):
 
         pygame.draw.rect(surface=surface,
                          color=BO_AQUAMARINE,
-                         rect=pygame.Rect(0, self._proportionality, self.width * self._proportionality, self.width * self._proportionality))
+                         rect=pygame.Rect(0, self._proportionality, self.width * self._proportionality,
+                                          self.width * self._proportionality))
 
         ################################################################
 
@@ -96,17 +96,17 @@ class GameBoard(Board):
         float_conversion = {self.width - a: a for a in range(self.width)}
 
         if isinstance(x, int) and isinstance(y, int):
-            new_x = x * self._proportionality + self._proportionality / 2
-            new_y = self._proportionality * int_conversion[y] + self._proportionality / 2
+            x = x * self._proportionality + self._proportionality / 2
+            y = self._proportionality * int_conversion[y] + self._proportionality / 2
 
         elif isinstance(x, float) and isinstance(y, float):
-            new_x = int((x - self._proportionality / 2) / self._proportionality)
-            new_y = float_conversion[int((y - self._proportionality / 2) / self._proportionality)]
+            x = int((x - self._proportionality / 2) / self._proportionality)
+            y = float_conversion[int((y - self._proportionality / 2) / self._proportionality)]
 
         else:
             raise ValueError
 
-        new_coordinate = (new_x, new_y)
+        new_coordinate = (x, y)
 
         return new_coordinate
 
@@ -162,6 +162,7 @@ class GameBoard(Board):
         if not isinstance(coordinates[0], float):
             coordinates = self.convert_coordinates(coordinates)
 
+        self.piece_drop_animation(coordinates[0], surface)
         pygame.draw.circle(surface=surface,
                            color=self.determine_colour(),
                            center=coordinates,
@@ -181,7 +182,8 @@ class GameBoard(Board):
 
         if self.get_winner() is not None:
             if self.get_winner()[0] != "Tie":
-                surface.fill(self.determine_colour(True))
+                # surface.fill(self.determine_colour(True))
+                self.fill_winner_colour(surface)
                 pygame.display.update()
                 print(self.get_winner()[0], "is the winner!")
                 pygame.quit()
@@ -191,6 +193,69 @@ class GameBoard(Board):
                 pygame.display.update()
                 print('Hey its a tie! Try again next time.')
                 pygame.quit()
+
+    def fill_winner_colour(self, surface: pygame.Surface) -> None:
+        """If a player has won, fill the winning sequence of moves with a darker colour."""
+
+        dark_red = (205, 92, 92)
+        dark_orange = (219, 143, 2)
+
+        player = self.get_winner()[0]
+        sequence = self.get_winner()[1]
+
+        if player == 'P1':
+
+            for circle in sequence:
+                coordinates = self.convert_coordinates(tuple(circle))
+                pygame.draw.circle(surface=surface,
+                                   color=dark_red,
+                                   center=coordinates,
+                                   radius=self._proportionality // 2 - 3)
+                pygame.display.update()
+        else:
+
+            for circle in sequence:
+                coordinates = self.convert_coordinates(tuple(circle))
+                pygame.draw.circle(surface=surface,
+                                   color=dark_orange,
+                                   center=coordinates,
+                                   radius=self._proportionality // 2 - 3)
+                pygame.display.update()
+
+    def piece_drop_animation(self, x_coordinate: float, surface: pygame.Surface) -> None:
+        """Add a dropping effect to where a game piece is being placed."""
+
+        y_coordinates = self.column_spaces[x_coordinate]
+        y_coordinates.sort()
+        for y in y_coordinates:
+            pygame.draw.circle(surface=surface,
+                               color=self.determine_colour(),
+                               center=(x_coordinate, y),
+                               radius=self._proportionality // 2 - 3)
+            pygame.display.update()
+            pygame.time.wait(80)
+            pygame.draw.circle(surface=surface,
+                               color=BA_LAVANDAR,
+                               center=(x_coordinate, y),
+                               radius=self._proportionality // 2 - 3)
+            pygame.display.update()
+
+    def draw_slider(self, coordinates: tuple[float, float], surface: pygame.Surface) -> None:
+        """Given the coordinates of the mouse position from the game while loop, create a
+        game picce slider that allows a player to see which token they are placing.
+
+        Preconditions:
+            - self.game_state == "PVP"
+
+        """
+
+        pygame.draw.rect(surface, BA_LAVANDAR, (0, 0, self.width * self._proportionality, self._proportionality))
+        pygame.draw.circle(surface=surface,
+                           color=self.determine_colour(),
+                           center=coordinates,
+                           radius=self._proportionality // 2 - 3)
+
+        pygame.display.update()
 
     def run_pvp_state(self) -> None:
         """Run a Player-versus-player game."""
@@ -211,25 +276,44 @@ class GameBoard(Board):
 
         self.draw_slots(game_surface)
 
+        slider_x = self._proportionality
+        slider_y = self._proportionality - (self._proportionality // 2)
+
         running = True
         while running:
+            self.check_winner(game_surface)
 
             clock.tick(60)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                if event.type == pygame.MOUSEMOTION:
+                    mouse_x = event.pos[0]
+                    if mouse_x < slider_x and slider_x > (self.width * self._proportionality) - (
+                            self.width * self._proportionality - 40):
+                        slider_x -= 10
+                    if mouse_x > slider_x and slider_x < (self.width * self._proportionality - 40):
+                        slider_x += 10
+
+                    self.draw_slider((slider_x, slider_y), game_surface)
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
 
                     clicks = [slot.collidepoint(event.pos) for slot in self.slots]
                     if any(clicks):
+                        self.check_winner(game_surface)
                         new_move = self.player_click_to_coordinates(clicks)
                         convert_cord = self.convert_coordinates(new_move)
                         game_piece = self.pieces[convert_cord]
                         self.visualize_and_record_move(game_piece, game_surface)
                         self.check_winner(game_surface)
-
-            pygame.display.update()
+            self.check_winner(game_surface)
+            try:
+                pygame.display.update()
+            except pygame.error:
+                return self.get_winner()
 
         quit()
 
@@ -258,7 +342,7 @@ class GameBoard(Board):
 
         running = True
         while running:
-
+            self.check_winner(game_surface)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -267,8 +351,8 @@ class GameBoard(Board):
 
             if not self.first_player_turn():
                 new_move = ai_player.make_move(self)
-                self.visualize_and_record_move(new_move, game_surface)
-                convert_move = self.convert_coordinates(new_move.location)
+                self.visualize_and_record_move(new_move[0], game_surface)
+                convert_move = self.convert_coordinates(new_move[0].location)
                 self.column_spaces[convert_move[0]].remove(convert_move[1])
                 self.check_winner(game_surface)
                 iteration += 1
@@ -287,10 +371,13 @@ class GameBoard(Board):
                             iteration += 1
 
                             if iteration >= 3:
-                                tree = generate_game_tree(self.player_moves['P1'][-1], self, 4)
+                                tree = generate_game_tree(self.player_moves['P1'][-1], self, 3)
                                 ai_player = GreedyPlayer(tree, 'P2')
-
-            pygame.display.update()
+            self.check_winner(game_surface)
+            try:
+                pygame.display.update()
+            except pygame.error:
+                return self.get_winner()
 
         quit()
 
@@ -305,3 +392,16 @@ class GameBoard(Board):
 
         else:
             print('Error: Game state is not specified')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'max-nested-blocks': 4,
+        'extra-imports': ['game_tree', 'board', 'pygame', 'players'],
+        'disable': ['unused-import', 'too-many-branches'],
+    })
